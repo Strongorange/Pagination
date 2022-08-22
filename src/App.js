5f569eb75205f0f276a8d352c70b16ef7ab47488
Chanhwi Lee <chanhwi.lee6@gmail.com>
2022-08-22 13:35:27 +0900

    Pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+import Pagination from "./Pagination";
+
+const Container = styled.div``;
+
+const getData = () => {
+  return axios.get("https://jsonplaceholder.typicode.com/posts");
+};
 
 function App() {
+  // States
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const { status, data } = useQuery("posts", getData);
+  const offset = (page - 1) * limit;
+
+  if (status === "loading") {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <label>
+        페이지 당 표시할 게시물 수:{" "}
+        <select
+          type="number"
+          value={limit}
+          onChange={({ target: { value } }) => setLimit(value)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
+      </label>
+
+      <main>
+        {data.data
+          .slice(offset, offset + limit)
+          .map(({ title, id, userId, body }) => (
+            <article key={id}>
+              <h3>
+                {id}. {title}
+              </h3>
+              <p>{body}</p>
+            </article>
+          ))}
+      </main>
+
+      <footer>
+        <Pagination
+          total={data.data.length}
+          limit={limit}
+          page={page}
+          setPage={setPage}
+        />
+      </footer>
+    </Container>
   );
 }
 
